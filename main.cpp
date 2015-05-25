30105caa82cdaaa465285a4f04f8179d708a79e2
Quantumlovag <lodi.peter@hallgato.ppke.hu>
2015-05-25 14:35:20 +0200

    Főmenü
    Főmenü működő inditó és kilépő gombbal

--- main.cpp
+++ main.cpp
@@ -12,25 +12,49 @@ using namespace genv;
 const int W=800;
 const int H=800;
 
-class MyApp : public Window
+class GameTable : public Window
 {
 protected:
     TicTac_Button *ttb1;
 public:
-    MyApp(int inX, int inY);
+    GameTable(int inX, int inY);
 };
 
-MyApp::MyApp(int inX, int inY)
+GameTable::GameTable(int inX, int inY)
     :Window(inX, inY)
 {
     ttb1=new TicTac_Button(0, 0, W, H);
     add(ttb1);
 }
 
+class MainMenu : public Window
+{
+protected:
+    Standard_Button *sb1;
+    Standard_Button *sb2;
+public:
+    MainMenu(int inX, int inY);
+};
+
+MainMenu::MainMenu(int inX, int inY)
+    :Window(inX, inY)
+{
+    sb1=new Standard_Button(100, 100, 100, 30, "Play", [&]()
+                            {
+                                this->shutdown();
+                                GameTable gt(W, H);
+                                gt.events();
+                            });
+    sb2=new Standard_Button(100, 200, 100, 30, "Exit", [&]()
+                            {
+                                this->shutdown();
+                            });
+    add(sb1), add(sb2);
+}
+
 int main()
 {
-    cout << "Hello World!";
-    MyApp app(W, H);
-    app.events();
+    MainMenu mm(300, 300);
+    mm.events();
     return 0;
 }
